0ef291450503dcbe5a4bf356fd5388632725f166
Oren Teich <oren@teich.net>
2026-02-19 18:05:51 -0800

    Initial Commit

---
+++ TestLog/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  TestLog
+//
+//  Created by Oren Teich on 2/19/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
